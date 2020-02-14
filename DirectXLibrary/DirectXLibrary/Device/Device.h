65dbfe8fb526deb294a17fa6ef80d92e2b8cdf15
mono1140 <11potato30@gmail.com>
2020-02-14 23:55:03 +0900

    ファイル階層の変更

---
+++ DirectXLibrary/DirectXLibrary/Device/Device.h
@@ -0,0 +1,36 @@
+﻿#ifndef DEVICE_H
+#define DEVICE_H
+
+#include <windows.h>
+#include <d3d9.h>
+
+#include "../Winodw/Window.h"
+
+namespace Library
+{
+
+	class Device
+	{
+	public:
+
+		Device()
+		{
+			Initialize();
+		}
+
+
+
+	private:
+
+		LPDIRECT3D9 pDirect3d;
+		LPDIRECT3DDEVICE9 pDevice;
+		D3DPRESENT_PARAMETERS d3dpp;
+
+		HRESULT Initialize();
+
+
+	};
+
+}
+
+#endif
