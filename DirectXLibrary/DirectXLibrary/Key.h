a6d0d96c8335b04b0eed54c20e7acce499fe2999
mono1140 <11potato30@gmail.com>
2020-02-17 00:53:07 +0900

    KeyClassの作成

---
+++ DirectXLibrary/DirectXLibrary/Key.h
@@ -0,0 +1,23 @@
+﻿#ifndef KEY_H
+#define KEY_H
+
+
+#include "../DirectXLibrary/Device/Device.h"
+
+namespace Library
+{
+	class Key : public Device
+	{
+	public:
+
+
+	private:
+
+
+
+	};
+
+}
+
+#endif
+
