9905a74879dc0fde8a5897b2fdc45422d265ea82
mono1140 <11potato30@gmail.com>
2020-02-17 00:53:36 +0900

    MouseClass の作成

---
+++ DirectXLibrary/DirectXLibrary/Mouse.h
@@ -0,0 +1,23 @@
+﻿#ifndef MOUSE_H
+#define MOUSE_H
+
+
+#include "../DirectXLibrary/Device/Device.h"
+
+namespace Library
+{
+	class Mouse : public Device
+	{
+	public:
+
+
+	private:
+
+
+
+	};
+
+}
+
+#endif
+
